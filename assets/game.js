662f2b0704cbb6ba3fa8f770ce5a70257f0ac184
Joel Brewster <hi@joelbrewster.com>
2017-01-31 16:14:05 +1100

    Screen update and notify

--- assets/game.js
+++ assets/game.js
@@ -7,6 +7,18 @@ var Game = {
   },
   getDisplay: function() {
     return this._display;
+  },
+  switchScreen: function(screen) {
+    //screen before, notify that exited
+    if (this._currentScreen !== null) {
+      this._currentScreen.exit();
+    }
+    //clear the display
+    this.getDisplay().clear();
+    if (!this._currentScreen !== null) {
+      this._currentScreen.enter();
+      this._currentScreen.render(this._display);
+    }
   }
 }
 window.onload = function() {
@@ -17,25 +29,5 @@ window.onload = function() {
     Game.init();
     // add container to the page
     document.body.appendChild(Game.getDisplay().getContainer());
-    // Create a 80 x 20 display
-		// var display = new ROT.Display({width:80, height:20});
-		// var container = display.getContainer();
-    //Add the container to the page
-		// document.body.appendChild(container);
-
-    // //Start of hello word to test out display.drawText
-		// var foreground, background, colors;
-		// for (var i = 0; i < 15; i++) {
-    //   //calculate foreground colors and get darker
-    //   //calculate background colors and get lighter
-			// foreground = ROT.Color.toRGB([255 - (i*20),
-			                              // 255 - (i*20),
-			                              // 255 - (i*20)]);
-			// background = ROT.Color.toRGB([i*20, i*20, i*20]);
-    //   // specify colors
-			// colors = "%c{" + foreground + "}%b{" + background + "}";
-    //   // draw the text at col 2, row 1
-			// display.drawText(2, i, colors + "Hello, world!");
-		// }
 	}
 }
