97b1e3d6f3e2bcaaff83a2eb00d95a581d5b8396
Joel Brewster <hi@joelbrewster.com>
2017-01-31 12:01:34 +1100

    Added container for display

--- assets/game.js
+++ assets/game.js
@@ -4,3 +4,9 @@ if (!ROT.isSupported()) {
 } else {
     // Good to go!
 }
+
+// Create a 80 x 20 display
+var display = new ROT.Display({width: 80, height: 20});
+var container = display.getContainer();
+//Add the container to the page
+document.body.appendChild(container);
