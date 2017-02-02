c4bb95b02d85c1a53d7f9a4c4d961c977c42f1bd
Joel Brewster <hi@joelbrewster.com>
2017-02-02 15:03:27 +1100

    Win and loose screen

--- assets/screens.js
+++ assets/screens.js
@@ -33,9 +33,9 @@ Game.Screen.playScreen = {
   },
   // Render the prompt to the screen
   render: function(display) {
-  display.drawText(3, 5, "%c{red}%b{white}Game started");
-  display.drawText(4, 6, "Press [Enter] to win, [Esc] to lose!");
-},
+    display.drawText(3, 5, "%c{red}%b{white}Game started");
+    display.drawText(4, 6, "Press [Enter] to win, [Esc] to lose!");
+  },
   handleInput: function(inputType, inputData) {
     if (inputType === 'keydown') {
       // If enter is pressed, go to the win screen
@@ -48,3 +48,39 @@ Game.Screen.playScreen = {
     }
   }
 }
+
+// Define our winning screen
+Game.Screen.winScreen = {
+  enter: function() {    console.log("Entered win screen."); },
+  exit: function() { console.log("Exited win screen."); },
+  render: function(display) {
+    // Render our prompt to the screen
+    for (var i = 0; i < 22; i++) {
+      // Generate random background colors
+      var r = Math.round(Math.random() * 255);
+      var g = Math.round(Math.random() * 255);
+      var b = Math.round(Math.random() * 255);
+      var background = ROT.Color.toRGB([r, g, b]);
+      display.drawText(2, i + 1, "%b{" + background + "}You win!");
+    }
+  },
+  handleInput: function(inputType, inputData) {
+    // Nothing to do here yet
+  }
+}
+
+// Define our losing screen
+Game.Screen.loseScreen = {
+  enter: function() {    console.log("Entered lose screen."); },
+  exit: function() { console.log("Exited lose screen."); },
+  render: function(display) {
+    // Render our prompt to the screen
+    for (var i = 0; i < 22; i++) {
+      display.drawText(2, i + 1, "%b{red}You lose! :(");
+    }
+  },
+  handleInput: function(inputType, inputData) {
+  // Nothing to do here
+  }
+}
+
