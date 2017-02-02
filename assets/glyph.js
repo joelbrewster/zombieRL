1ff8d46ede39315f143da3b8c44ec994f7964345
Joel Brewster <hi@joelbrewster.com>
2017-02-02 15:40:40 +1100

    Game glyphs - foreground and background

---
+++ assets/glyph.js
@@ -0,0 +1,16 @@
+Game.Glyph = function(chr, foreground, background) {
+  //set properties to default if they weren't passed
+  this._char = chr || ' ';
+  this._foreground = foreground || 'white';
+  this._background = background || 'black';
+};
+
+//create standard getters for glyphs
+Game.Glyph.prototype.getChar = function() {
+  return this._char;
+}
+Game.Glyph.prototype.getBackground = function() {
+  return this._background;
+}
+Game.Glyph.prototype.getForeground = function() {
+
